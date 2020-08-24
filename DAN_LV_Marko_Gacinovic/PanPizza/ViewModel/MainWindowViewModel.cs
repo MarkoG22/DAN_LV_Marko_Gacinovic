98884d00ba285b2dfb57a4b8d091e66dff5fdba1
MarkoG22 <marko.gacinovic.el@gmail.com>
2020-08-24 13:06:08 +0200

    MainWindow.xaml

---
+++ DAN_LV_Marko_Gacinovic/PanPizza/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanPizza.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        MainWindow main;
+
+
+    }
+}
